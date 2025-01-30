1603c11fdb578ed52b7632594c9d1f65f9f13c26
Nick Haretakis <haretakis4@gmail.com>
2025-01-29 22:30:44 -0800

    fixed t

--- index.js
+++ index.js
@@ -1,38 +1,46 @@
-const fs = require("fs")
+const fs = require("fs");
 
 const saveLocation = "../../testy/saveSpot";
 const exportFolders = "../../testy/";
 
 function fileListener() {
+  try {
     fs.watch(saveLocation, (eventType, filename) => {
       if (eventType == "rename") {
         bunnyProtocol(filename);
       }
     });
+  } catch (err) {
+    throw err;
+  }
 }
-
 
 function bunnyProtocol(newFile) {
-    fs.readdir(exportFolders, (err, files) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        files.forEach(folder => {
+  fs.readdir(exportFolders, (err, files) => {
+    if (err) {
+      console.log("file deleted");
+      return;
+    }
+    files.forEach((folder) => {
+      if (folder == "saveSpot") {
+        return;
+      }
 
-            if (folder == "saveSpot") {
-                return;
+      fs.copyFile(
+        saveLocation + "/" + newFile,
+        exportFolders + folder + "/" + newFile,
+        (err) => {
+          if (err) {
+            if (err.errno == "-2") {
+              return;
             }
-
-
-            fs.copyFile(saveLocation + "/" + newFile, exportFolders + folder + "/" + newFile, (err) => {
-                if (err) throw err;
-                console.log('exported');
-            })
-        })
-    })
+            console.log(err);
+          }
+          console.log("exported");
+        }
+      );
+    });
+  });
 }
 
-
-
-module.exports = fileListener
+module.exports = fileListener;
